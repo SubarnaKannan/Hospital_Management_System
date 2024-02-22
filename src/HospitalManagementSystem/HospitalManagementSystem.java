dc8d95e7ee47d0bfa79ac5d65049e1937a6770e3
Subarna Kannan <subarnakannan04@gmail.com>
2024-02-22 12:17:28 +0530

    second commit

--- src/HospitalManagementSystem/HospitalManagementSystem.java
+++ src/HospitalManagementSystem/HospitalManagementSystem.java
@@ -46,6 +46,7 @@ public class HospitalManagementSystem {
 
                     case 4:
                         //Book Appointments
+
                         bookAppointment(patient, doctor, connection, scanner);
                         return;
 
@@ -63,38 +64,44 @@ public class HospitalManagementSystem {
         }
     }
     public static void bookAppointment(Patient patient, Doctor doctor, Connection connection, Scanner scanner) {
-        System.out.print("Enter Patient Id: ");
-        int patientId = scanner.nextInt();
-        System.out.print("Enter Doctor Id: ");
-        int doctorId = scanner.nextInt();
-        System.out.print("Enter appointment date(YYYY-MM-DD): ");
-        String appointmentDate = scanner.next();
-        if(checkDoctorAvailability(doctorId, appointmentDate, connection)) {
-            if(patient.getPatientById(patientId) && doctor.getDoctorById(doctorId)) {
-                String appointmentQuery = "INSERT INTO appointments(patient_id, doctor_id, appointment_date) VALUES(?, ?, ?)";
-                try {
-                    PreparedStatement preparedStatement = connection.prepareStatement(appointmentQuery);
-                    preparedStatement.setInt(1, patientId);
-                    preparedStatement.setInt(2, doctorId);
-                    preparedStatement.setString(3, appointmentDate);
-                    int rowsAffected = preparedStatement.executeUpdate();
-                    if(rowsAffected>0) {
-                        System.out.println("Appointment Booked!!");
-                    }
-                    else {
-                        System.out.println("Failed to Book Appointment!!!");
+        boolean docAvail = false;
+        patient.viewPatients();
+        doctor.viewDoctors();
+        while(!docAvail) {
+            System.out.print("Enter Patient Id: ");
+            int patientId = scanner.nextInt();
+            System.out.print("Enter Doctor Id: ");
+            int doctorId = scanner.nextInt();
+            System.out.print("Enter appointment date(YYYY-MM-DD): ");
+            String appointmentDate = scanner.next();
+
+            if (checkDoctorAvailability(doctorId, appointmentDate, connection)) {
+                if (patient.getPatientById(patientId) && doctor.getDoctorById(doctorId)) {
+                    String appointmentQuery = "INSERT INTO appointments(patient_id, doctor_id, appointment_date) VALUES(?, ?, ?)";
+                    try {
+                        PreparedStatement preparedStatement = connection.prepareStatement(appointmentQuery);
+                        preparedStatement.setInt(1, patientId);
+                        preparedStatement.setInt(2, doctorId);
+                        preparedStatement.setString(3, appointmentDate);
+                        int rowsAffected = preparedStatement.executeUpdate();
+                        if (rowsAffected > 0) {
+                            System.out.println("Appointment Booked!!");
+                            docAvail = true;
+                            continue;
+                        }
+                    } catch (SQLException e) {
+                        e.printStackTrace();
                     }
                 }
-                catch(SQLException e) {
-                    e.printStackTrace();
+                 else {
+                    System.out.println("Either Doctor or Patient doesn't exist!!!");
+                    System.out.println("Please try again with valid data!!");
                 }
+            } else {
+                System.out.print("Doctor is not available on this date!!");
+                System.out.println("Please try again with valid data!!");
+
             }
-            else {
-                System.out.println("Doctor is not available on this date!!");
-            }
-        }
-        else {
-            System.out.println("Either Doctor or Patient doesn't exist!!!");
         }
     }
     public static boolean checkDoctorAvailability(int doctorId, String appointmentDate, Connection connection) {
